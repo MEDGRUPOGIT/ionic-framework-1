7dcc3186390711e09319192144a9d54ef800d0b3
Marcelo Brandao <marcelo.barbosa@medgrupo.com.br>
2021-07-15 16:20:58 -0300

    Ajuste css

--- core/src/components/medgrupo/01-core/med-offline/med-offline.tsx
+++ core/src/components/medgrupo/01-core/med-offline/med-offline.tsx
@@ -7,17 +7,21 @@ import { Component, Host, h } from '@stencil/core';
 })
 export class MedOffline {
 
+  private onClick(e:Event) {
+    e.preventDefault();
+  }
+
   render() {
     return (
-      <Host>
-        <slot></slot>
-        {/* <ion-icon class="icon" name="med-offline"></ion-icon> */}
-        <svg class="icon" viewBox="0 0 24 24">
-          <path class="icon-path" d="M22.61 16.95A4.999 4.999 0 0018 10h-1.26a8 8 0 00-7.05-6M5 5a8 8 0 004 15h9a5 5 0 001.7-.3"/>
-          <path class="icon-path" d="M1 1l22 22"/>
-        </svg>
-        <p class="title">Você está offline</p>
-        <p class="text">Conecte-se à internet para visualizar esse conteúdo</p>
+      <Host onClick={(e:Event) => this.onClick(e)}>
+        <div class="wrapper">
+          <svg class="icon" viewBox="0 0 24 24">
+            <path class="icon-path" d="M22.61 16.95A4.999 4.999 0 0018 10h-1.26a8 8 0 00-7.05-6M5 5a8 8 0 004 15h9a5 5 0 001.7-.3"/>
+            <path class="icon-path" d="M1 1l22 22"/>
+          </svg>
+          <p class="title">Você está offline</p>
+          <p class="text">Conecte-se à internet para visualizar esse conteúdo</p>
+        </div>
       </Host>
     );
   }
