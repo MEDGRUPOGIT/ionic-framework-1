247d1fd28b875e0d1729cac65baf8ca2cc22db6b
Marcelo Brandao <marcelo.barbosa@medgrupo.com.br>
2021-07-16 11:12:34 -0300

    Ajuste componente offlien

--- core/src/components/medgrupo/01-core/med-offline/med-offline.tsx
+++ core/src/components/medgrupo/01-core/med-offline/med-offline.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, Event, EventEmitter } from '@stencil/core';
 
 @Component({
   tag: 'med-offline',
@@ -7,20 +7,31 @@ import { Component, Host, h } from '@stencil/core';
 })
 export class MedOffline {
 
-  private onClick(e:Event) {
-    e.preventDefault();
+  @Event() medClick!: EventEmitter<void>;
+
+  private onClick() {
+    this.medClick.emit();
   }
 
   render() {
     return (
-      <Host onClick={(e:Event) => this.onClick(e)}>
+      <Host>
         <div class="wrapper">
-          <svg class="icon" viewBox="0 0 24 24">
-            <path class="icon-path" d="M22.61 16.95A4.999 4.999 0 0018 10h-1.26a8 8 0 00-7.05-6M5 5a8 8 0 004 15h9a5 5 0 001.7-.3"/>
-            <path class="icon-path" d="M1 1l22 22"/>
-          </svg>
-          <p class="title">Você está offline</p>
-          <p class="text">Conecte-se à internet para visualizar esse conteúdo</p>
+          <med-header>
+            <med-navbar ds-name="transparent" slot="navbar">
+              <ion-button slot="left" ds-name="icon-only" onClick={() => this.onClick()}>
+                <ion-icon slot="icon-only" name="med-chevron-left"></ion-icon>
+              </ion-button>
+            </med-navbar>
+          </med-header>
+          <div class="wrapper__content">
+            <svg class="icon" viewBox="0 0 24 24">
+              <path class="icon-path" d="M22.61 16.95A4.999 4.999 0 0018 10h-1.26a8 8 0 00-7.05-6M5 5a8 8 0 004 15h9a5 5 0 001.7-.3"/>
+              <path class="icon-path" d="M1 1l22 22"/>
+            </svg>
+            <p class="title">Você está offline</p>
+            <p class="text">Conecte-se à internet para visualizar esse conteúdo</p>
+          </div>
         </div>
       </Host>
     );
