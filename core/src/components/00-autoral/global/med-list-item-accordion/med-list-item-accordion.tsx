dc8b8a59c3586eacc319cd27351548f719d27874
alexbleggi <alexbleggi@gmail.com>
2021-07-30 11:11:27 -0300

    commit

--- core/src/components/00-autoral/global/med-list-item-accordion/med-list-item-accordion.tsx
+++ core/src/components/00-autoral/global/med-list-item-accordion/med-list-item-accordion.tsx
@@ -13,40 +13,41 @@ export class MedListItemAccordion {
   @Prop() label?: string;
   @Prop() neutral?: Neutral;
   @Prop() color?: Color;
-  @Prop({ reflect:true }) selected = false;
+  @Prop({ reflect: true }) selected = false;
   @Prop() dsSize?: 'xs' | 'sm' | 'md';
   @Prop() margin?: 'xs' | 'sm' | 'md' | 'lg';
-  @Prop({ reflect:true }) border = false;
+  @Prop({ reflect: true }) border = false;
   @Prop({ reflect: true, mutable: true }) collapsed = true;
+
   @Method()
-   async toggle(event?: Event) {
-     event?.stopPropagation();
-     this.collapsed = !this.collapsed;
-   }
+  async toggle(event?: Event) {
+    event?.stopPropagation();
+    this.collapsed = !this.collapsed;
+  }
 
   render() {
     const { color, neutral, titulo, label, selected, dsSize, border, margin, collapsed } = this;
+
     return (
       <Host
-      from-stencil
-      class={createColorClasses(color, {
-        'med-list-item-accordion': true,
-        'med-list-item-accordion--selected': selected,
-        'med-list-item-accordion--border-radius': border,
-        [`med-list-item-accordion--${dsSize}`]: dsSize !== undefined,
-        [`med-list-item-accordion--${margin}`]: margin !== undefined,
-        'med-list-item-accordion--collapsed': collapsed
-      }, neutral)}
-      >
+        from-stencil
+        class={createColorClasses(color, {
+          'med-list-item-accordion': true,
+          'med-list-item-accordion--selected': selected,
+          'med-list-item-accordion--border-radius': border,
+          [`med-list-item-accordion--${dsSize}`]: dsSize !== undefined,
+          [`med-list-item-accordion--${margin}`]: margin !== undefined,
+          'med-list-item-accordion--collapsed': collapsed
+        }, neutral)}>
         <div class="med-list-item-accordion__item">
           <slot name="start"></slot>
+
           <div class="med-list-item-accordion__content">
             <h3 class="med-list-item-accordion__title">{titulo}</h3>
             <h4 class="med-list-item-accordion__label">{label}</h4>
           </div>
-          <ion-button ds-name="icon-only" slot="left"
-          onClick={(event: any) => {this.toggle(event)}}
-          >
+
+          <ion-button ds-name="icon-only" slot="left" onClick={(event: any) => {this.toggle(event)}}>
             <ion-icon class="med-icon" name="med-baixo"></ion-icon>
           </ion-button>
         </div>
