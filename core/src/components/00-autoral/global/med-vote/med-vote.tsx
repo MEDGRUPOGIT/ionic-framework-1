99cc17ad146383dee347fc1316f861470a01cd9c
Leonardo Setubal <setubal43@gmail.com>
2021-08-16 14:49:00 -0300

    feat: correções e melhorias no med-vote

--- core/src/components/00-autoral/global/med-vote/med-vote.tsx
+++ core/src/components/00-autoral/global/med-vote/med-vote.tsx
@@ -11,30 +11,25 @@ export class MedVote {
   /**
     * Define o conteúdo de texto do componente.
     */
-  @Prop() titulo?: string;
+  @Prop() title?: string;
 
   /**
     * Define o conteúdo de texto do componente.
     */
-  @Prop() cabe?: number;
+  @Prop() like: number = 0;
 
   /**
     * Define o conteúdo de texto do componente.
     */
-  @Prop() naoCabe?: number;
-
-  public validarValores(_cabe:any,_naoCabe:any){
-    let valueCabe = _cabe >= 0 && _cabe !== undefined && _cabe !== null ? _cabe : 0
-    let valueNaoCabe = _naoCabe >= 0  && _naoCabe !== undefined && _naoCabe !== null ? _naoCabe : 0
-    return {valueCabe,valueNaoCabe}
-  }
+  @Prop() unlike: number = 0;
 
   render() {
-    const { titulo, cabe, naoCabe } = this;
-    const {valueCabe,valueNaoCabe} = this.validarValores(cabe,naoCabe)
-    const total = valueCabe + valueNaoCabe
-    const cabeP = ((valueCabe*100)/total)
-    const naoCabeP = ((valueNaoCabe*100)/total)
+    const title =  this.title
+    const like = isNaN(this.like) ? 0 : +this.like;
+    const unlike = isNaN(this.unlike) ? 0 : +this.unlike;
+    const total = like + unlike
+    const likePercent = ((like*100)/total)
+    const unlikePercent = ((unlike*100)/total)
     return (
       <Host
         from-stencil
@@ -44,17 +39,17 @@ export class MedVote {
         <div class="med-vote__row">
           <div class="med-vote__icon-container">
             <ion-icon class="med-icon med-vote__icon med-vote__icon--cabe" name="med-positivo"></ion-icon>
-            <div class="med-vote__badge med-vote__badge--cabe">{valueCabe}</div>
+            <div class="med-vote__badge med-vote__badge--cabe">{like}</div>
           </div>
-          <h3 class="med-vote__heading" innerHTML={titulo}></h3>
+          <h3 class="med-vote__heading" innerHTML={title}></h3>
           <div class="med-vote__icon-container">
-            <div class="med-vote__badge med-vote__badge--nao-cabe">{valueNaoCabe}</div>
+            <div class="med-vote__badge med-vote__badge--nao-cabe">{unlike}</div>
             <ion-icon class="med-icon med-vote__icon med-vote__icon--nao-cabe" name="med-negativo"></ion-icon>
           </div>
         </div>
         <div class="med-vote__row">
-          <div class="med-vote__chart med-vote__chart--cabe" style={{ width: `${cabeP}%` }}></div>
-          <div class="med-vote__chart med-vote__chart--nao-cabe" style={{ width: `${naoCabeP}%` }}></div>
+          <div class="med-vote__chart med-vote__chart--cabe" style={{ width: `${likePercent}%` }}></div>
+          <div class="med-vote__chart med-vote__chart--nao-cabe" style={{ width: `${unlikePercent}%` }}></div>
         </div>
       </Host>
     );
