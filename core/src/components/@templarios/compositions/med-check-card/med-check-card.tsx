ff79b0428d338655a1b5de9d433631e1ca683c42
Valdir-Junior10 <valdir.conceicaojr@gmail.com>
2022-08-22 15:42:13 +0100

    Squashed commit of the following:
    commit c8c14cb18e2fc85a08299ba3f9b67e7eaed0db40
    Author: Valdir-Junior10 <valdir.conceicaojr@gmail.com>
    Date:   Mon Aug 22 15:16:58 2022 +0100
    
        ajustes med-check-card

--- core/src/components/@templarios/compositions/med-check-card/med-check-card.tsx
+++ core/src/components/@templarios/compositions/med-check-card/med-check-card.tsx
@@ -1,14 +1,13 @@
-import { Component, Event, EventEmitter, h, Host, Prop } from '@stencil/core';
-import { MedColor } from '../../../../@templarios/types/color.type';
-import { generateMedColor } from '../../../../@templarios/utilities/color';
+import { Component, Event, EventEmitter, h, Host, Prop } from "@stencil/core";
+import { MedColor } from "../../../../@templarios/types/color.type";
+import { generateMedColor } from "../../../../@templarios/utilities/color";
 
 @Component({
-  tag: 'med-check-card',
-  styleUrl: 'med-check-card.scss',
-  shadow: true,
+  tag: "med-check-card",
+  styleUrl: "med-check-card.scss",
+  scoped: true,
 })
 export class MedCheckCard {
-
   /**
    * todo
    */
@@ -78,15 +77,26 @@ export class MedCheckCard {
   }
 
   render() {
-    const { dsColor, alert, titulo, categoria, horaInicial, horaFinal, dataInicial, dataFinal, iconName } = this;
+    const {
+      dsColor,
+      alert,
+      titulo,
+      categoria,
+      horaInicial,
+      horaFinal,
+      dataInicial,
+      dataFinal,
+      iconName,
+    } = this;
 
     return (
       <Host
         from-stencil
         class={generateMedColor(dsColor, {
-          'med-check-card': true,
-          'med-check-card--alert': alert,
-        })}>
+          "med-check-card": true,
+          "med-check-card--alert": alert,
+        })}
+      >
         <med-base spacing-v="s00" spacing-h="s00" class="med-check-card__base">
           <div class="med-check-card__input-container">
             <slot name="input"></slot>
@@ -94,27 +104,45 @@ export class MedCheckCard {
 
           <div class="med-check-card__container" onClick={() => this.onClick()}>
             <div class="med-check-card__text-container">
-              <med-type class="med-check-card__title" token="p16xb">{titulo}</med-type>
+              <med-type class="med-check-card__title" token="p16xb">
+                {titulo}
+              </med-type>
               <div class="med-check-card__title-wrap">
                 <div class="med-check-card__info-container">
                   <div class="med-check-card__container-fix">
-                    <ion-icon class="med-check-card__icon med-icon med-icon--xs" name={iconName}></ion-icon>
-                    <med-type class="med-check-card__subtitulo" token="p12xb">{categoria}</med-type>
+                    <ion-icon
+                      class="med-check-card__icon med-icon med-icon--xs"
+                      name={iconName}
+                    ></ion-icon>
+                    <med-type class="med-check-card__subtitulo" token="p12xb">
+                      {categoria}
+                    </med-type>
                   </div>
-                  {!dataInicial && !dataFinal && horaInicial && horaFinal && <med-type class="med-check-card__hora" token="p12x">{horaInicial} – {horaFinal}</med-type>}
-                  {dataInicial && dataFinal && !horaInicial && !horaFinal && <med-type class="med-check-card__data" token="p12x">{dataInicial} até {dataFinal}</med-type>}
+                  {!dataInicial && !dataFinal && horaInicial && horaFinal && (
+                    <med-type class="med-check-card__hora" token="p12x">
+                      {horaInicial} – {horaFinal}
+                    </med-type>
+                  )}
+                  {dataInicial && dataFinal && !horaInicial && !horaFinal && (
+                    <med-type class="med-check-card__data" token="p12x">
+                      {dataInicial} até {dataFinal}
+                    </med-type>
+                  )}
                 </div>
               </div>
-              {dataInicial && dataFinal && horaInicial && horaFinal && <med-type class="med-check-card__data-hora" token="p12x">{dataInicial} - {horaInicial} até {dataFinal} - {horaFinal}</med-type>}
+              {dataInicial && dataFinal && horaInicial && horaFinal && (
+                <med-type class="med-check-card__data-hora" token="p12x">
+                  {dataInicial} - {horaInicial} até {dataFinal} - {horaFinal}
+                </med-type>
+              )}
             </div>
           </div>
 
-          <div class={alert ? 'med-check-card__tooltip-container' : ''}>
+          <div class={alert ? "med-check-card__tooltip-container" : ""}>
             <slot name="tooltip"></slot>
           </div>
         </med-base>
       </Host>
     );
   }
-
 }
