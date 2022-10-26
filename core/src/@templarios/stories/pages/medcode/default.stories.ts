23c0c3a357ed277c5e1f8afca5c0a59dea8c0ca7
alanfedrizzi <alanfedrizzi@gmail.com>
2022-10-26 17:28:15 -0300

    migracao ionic 1 tela medcode

--- core/src/@templarios/stories/pages/medcode/default.stories.ts
+++ core/src/@templarios/stories/pages/medcode/default.stories.ts
@@ -8,7 +8,32 @@ export default {
 
 const Template = () => {
   return html`
-    <h1>Medcode</h1>
+    <div class="container">
+      <header class="header">
+        <div class="header__left">
+        <ion-back-button class="header__back-button" mode="ios" text="" ds-size="sm">
+          <ion-icon class="med-icon" name="med-esquerda"></ion-icon>
+        </ion-back-button>
+        </div>
+        <div class="header__middle">
+          <ion-label class="header__title" ds-color="neutral-10" token="p16xb">MEDCODE</ion-label>
+        </div>
+        <div class="header__right">
+        <ion-button fill="clear" mode="ios" icon-only ds-size="sm">
+          <ion-icon class="med-icon" slot="icon-only" name="med-qrcode"></ion-icon>
+        </ion-button>
+        </div>
+      </header>
+
+      <main class="content-medcode">
+        <div class="content-medcode__container">
+          <ion-label class="content-medcode__text" ds-color="neutral-10" token="p18x">Para ler uma marca, clique aqui:</ion-label>
+          <ion-button class="content-medcode__button" fill="clear" mode="ios" icon-only>
+            <ion-icon class="med-icon" slot="icon-only" name="med-qrcode"></ion-icon>
+          </ion-button>
+        </div>
+      </main>
+    </div>
   `;
 };
 
