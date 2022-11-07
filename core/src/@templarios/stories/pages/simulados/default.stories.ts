061a95d5d257aabebcc3717d235942d0154b5856
alanfedrizzi <alanfedrizzi@gmail.com>
2022-11-07 17:30:28 -0300

    tela simulados resumo

--- core/src/@templarios/stories/pages/simulados/default.stories.ts
+++ core/src/@templarios/stories/pages/simulados/default.stories.ts
@@ -17,95 +17,87 @@ const Template = () => {
 
           <ion-label class="header__title" slot="title" token="p16xb">Simulados</ion-label>
         </med-navbar>
-
-        <med-toolbar class="subheader" slot="toolbar">
-          <ion-label class="subheader__title" ds-color="neutral-10" token="p16xb">2022</ion-label>
-        </med-toolbar>
       </med-header>
 
       <main class="simulados">
-        <div class="simulados__container">
-          <div class="simulados__grid">
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 01" label="Geral 01">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 02" label="Geral 02">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 03" label="Geral 03">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 04" label="Geral 04">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 05" label="Geral 05">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 06" label="Geral 06">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 07" label="Geral 07">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-          </div>
+        <ion-label class="simulados__ano" ds-color="neutral-10" token="p16xb">2022</ion-label>
+        <div class="simulados__grid">
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 01" label="Geral 01">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 02" label="Geral 02">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 03" label="Geral 03">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 04" label="Geral 04">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 05" label="Geral 05">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 06" label="Geral 06">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 07" label="Geral 07">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
         </div>
 
-        <div class="simulados__container">
-          <ion-label class="simulados__ano" ds-color="neutral-10" token="p16xb">2021</ion-label>
-          <div class="simulados__grid">
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 01" label="Geral 01">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 02" label="Geral 02">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 03" label="Geral 03">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 04" label="Geral 04">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 05" label="Geral 05">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 06" label="Geral 06">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 07" label="Geral 07">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="SIM EXTRA" label="QUESTÕES COVID">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-          </div>
+        <ion-label class="simulados__ano" ds-color="neutral-10" token="p16xb">2021</ion-label>
+        <div class="simulados__grid">
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 01" label="Geral 01">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 02" label="Geral 02">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 03" label="Geral 03">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 04" label="Geral 04">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 05" label="Geral 05">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 06" label="Geral 06">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 07" label="Geral 07">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="SIM EXTRA" label="QUESTÕES COVID">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
         </div>
 
-        <div class="simulados__container">
-          <ion-label class="simulados__ano" ds-color="neutral-10" token="p16xb">2020</ion-label>
-          <div class="simulados__grid">
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 01" label="Geral 01">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 02" label="Geral 02">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 03" label="Geral 03">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 04" label="Geral 04">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 05" label="Geral 05">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 06" label="Geral 06">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-            <med-tiles class="simulados__tiles" titulo="R01 SIM 07" label="Geral 07">
-              <ion-badge ds-size="xs">Estude</ion-button>
-            </med-tiles>
-          </div>
+        <ion-label class="simulados__ano" ds-color="neutral-10" token="p16xb">2020</ion-label>
+        <div class="simulados__grid">
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 01" label="Geral 01">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 02" label="Geral 02">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 03" label="Geral 03">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 04" label="Geral 04">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 05" label="Geral 05">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 06" label="Geral 06">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
+          <med-tiles class="simulados__tiles" titulo="R01 SIM 07" label="Geral 07">
+            <ion-badge ds-size="xs" ds-color="fb-attention">Estude</ion-button>
+          </med-tiles>
         </div>
+
       </main>
     </div>
   `;
---
+++ core/src/@templarios/stories/pages/simulados/resumo.stories.ts
@@ -0,0 +1,193 @@
+import { html } from 'lit-html';
+import { withDesign } from 'storybook-addon-designs';
+
+export default {
+  title: 'Pages/Medsoft/Simulados',
+  decorators: [withDesign],
+};
+
+const Template = ( {valoresChart, valoresLabel} ) => {
+
+  setTimeout(() => {
+    document.querySelector('med-chart-radial').valores = valoresChart.valoresChart;
+  }, 1000);
+
+  setTimeout(() => {
+    document.querySelector('med-chart-radial-label').valores = valoresLabel.valoresLabel;
+  }, 1000);
+
+  return html`
+    <div class="container">
+      <med-header>
+        <med-navbar class="header" slot="navbar">
+          <ion-back-button slot="left" mode="ios" text="" ds-size="xxs">
+            <ion-icon class="med-icon" name="med-esquerda"></ion-icon>
+          </ion-back-button>
+
+          <ion-label class="header__title" slot="title" token="p16xb">2022 R1
+          SIM 01</ion-label>
+          <ion-label class="header__subtitle" slot="subtitle" token="p14x">Geral 01</ion-label>
+        </med-navbar>
+      </med-header>
+
+      <main class="resumo">
+        <ion-item ds-color="brand" spacing-v="s00" spacing-h="s04"     mode="ios" lines="none">
+        <ion-button class="resumo__button" mode="ios" fill="clear" ds-color="neutral-10" ds-size="xxs">
+          <ion-icon slot="start" class="med-icon" name="med-trofeu"></ion-icon>
+            REALIZADO MODO PROVA (EM 20/10/2022)
+          <ion-icon slot="end" class="med-icon" name="med-baixo"></ion-icon>
+        </ion-button>
+        </ion-item>
+
+        <div class="resumo__classificacao">
+          <div class="resumo__linha">
+            <ion-label token="p12x">Qual seria sua classificação &nbsp;</ion-label>
+            <ion-button class="resumo__opcoes" mode="ios" fill="clear" ds-size="xxs" no-padding="true">
+              no Brasil
+            </ion-button>
+          </div>
+
+          <div class="resumo__linha">
+            <ion-label token="p12x">&nbsp; em &nbsp;</ion-label>
+            <ion-button class="resumo__opcoes" mode="ios" fill="clear" ds-size="xxs" no-padding="true">
+              Todas as Unidades
+            </ion-button>
+          </div>
+
+          <div class="resumo__linha">
+            <ion-label token="p12x">&nbsp; em &nbsp;</ion-label>
+            <ion-button class="resumo__opcoes" mode="ios" fill="clear" ds-size="xxs" no-padding="true">
+              Todas as Especialidades
+            </ion-button>
+          </div>
+        </div>
+
+        <div class="resumo__range-container">
+          <ion-icon class="med-icon resumo__icon resumo__icon--start" name="med-usuario"></ion-icon>
+          <div class="resumo__progress">
+            <ion-range class="resumo__range" min="20" max="80" step="2" ds-color="fb-warning">
+            </ion-range>
+            <div class="resumo__posicao-container" style="--porcentagem: 50">
+            <ion-label class="resumo__posicao" token="p14xx"> 100° </ion-label>
+            <ion-label class="resumo__sua-nota" token="p10"> Sua nota </ion-label>
+            <ion-label class="resumo__nota" token="p12xb"> 5.0 </ion-label>
+          </div>
+          </div>
+          <ion-icon class="med-icon resumo__icon resumo__icon--end" name="med-acertou"></ion-icon>
+        </div>
+
+        <div class="resumo__participantes">
+          <ion-label token="p14x" ds-color="neutral-8">Total de</ion-label>
+          <ion-label token="p14xb" ds-color="neutral-8">&nbsp; 15000 &nbsp;</ion-label>
+          <ion-label token="p14x" ds-color="neutral-8">participantes</ion-label>
+        </div>
+
+        <div class="resumo__questoes">
+          <med-chart-radial titulo="100" subtitulo="Questoes" ds-size="lg"></med-chart-radial>
+          <med-chart-radial-label></med-chart-radial-label>
+        </div>
+
+        <ion-item ds-color="brand" spacing-v="s00" spacing-h="s04"     mode="ios" lines="none">
+        <ion-button class="resumo__button" mode="ios" fill="clear" ds-color="neutral-10" ds-size="xxs">
+          <ion-icon slot="start" class="med-icon" name="med-apostila"></ion-icon>
+            MODO PROVA
+          </ion-button>
+        </ion-item>
+
+        <div class="resumo__questoes">
+          <med-chart-radial titulo="100" subtitulo="Questoes" ds-size="lg"></med-chart-radial>
+          <med-chart-radial-label></med-chart-radial-label>
+        </div>
+
+        <ion-item ds-color="brand" spacing-v="s00" spacing-h="s04"     mode="ios" lines="none">
+        <ion-button class="resumo__button" mode="ios" fill="clear" ds-color="neutral-10" ds-size="xxs">
+          <ion-icon slot="start" class="med-icon" name="med-apostila"></ion-icon>
+            MODO ESTUDO
+        </ion-button>
+        </ion-item>
+
+        <div class="resumo__questoes">
+          <med-chart-radial titulo="100" subtitulo="Questoes" ds-size="lg"></med-chart-radial>
+          <med-chart-radial-label></med-chart-radial-label>
+        </div>
+      </main>
+    </div>
+  `;
+};
+
+export const Resumo = Template.bind({});
+Resumo.parameters = {
+  design: {
+    type: 'figma',
+    url: '',
+  },
+}
+Resumo.argTypes = {
+  valoresChart: {
+    description: "Define os valores do gráfico.",
+    defaultValue: {
+      valoresChart: [
+        {
+          cor: 'med-color-fb-success',
+          label: 'acertos',
+          quantia: 15,
+          ignoreBarra: false,
+        },
+        {
+          cor: 'med-color-fb-caution',
+          label: 'acertos',
+          quantia: 30,
+          ignoreBarra: false,
+       },
+       {
+          cor: 'med-color-revalida',
+          label: 'teste',
+          quantia: 45,
+          ignoreBarra: false,
+      },
+       {
+         cor: '',
+          label: 'restantes',
+          quantia: 10,
+          ignoreBarra: true,
+        }
+      ],
+    },
+    control: { type: 'array' },
+    description: 'Define a lista...',
+    table: {
+      type:  { summary: 'MedRadialItem[]' },
+      defaultValue: { summary: 'undefined' },
+    },
+  },
+  valoresLabel: {
+    defaultValue: {
+      valoresLabel: [
+        {
+          cor: 'med-color-fb-success',
+          label: 'Acertos',
+          quantia: 32,
+          ignoreBarra: false,
+        },
+        {
+          cor: 'med-color-fb-warning',
+          label: 'Erros',
+          quantia: 16,
+          ignoreBarra: false,
+        },
+        {
+          cor: '',
+          label: 'Restantes',
+          quantia: 52,
+          ignoreBarra: true,
+        }
+      ],
+    },
+    control: { type: 'array' },
+    description: 'Define a lista...',
+    table: {
+      type:  { summary: 'MedRadialItem[]' },
+      defaultValue: { summary: 'undefined' },
+    },
+  },
+};
