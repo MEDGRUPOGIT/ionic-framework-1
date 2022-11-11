cabb40370edca99ad8f288327a881ae37485d36b
alanfedrizzi <alanfedrizzi@gmail.com>
2022-11-11 16:55:55 -0300

    ajuste ion-content medcode

--- core/src/@templarios/stories/pages/medcode/default.stories.ts
+++ core/src/@templarios/stories/pages/medcode/default.stories.ts
@@ -23,14 +23,16 @@ const Template = () => {
       </med-navbar>
     </med-header>
 
-      <main class="content medcode">
-        <div class="medcode__container">
-          <ion-label class="medcode__text" ds-color="neutral-10" token="p18x">Para ler uma marca, clique aqui:</ion-label>
-          <ion-button class="medcode__button" fill="clear" mode="ios" icon-only>
-            <ion-icon class="med-icon" slot="icon-only" name="med-qrcode"></ion-icon>
-          </ion-button>
-        </div>
-      </main>
+      <ion-content>
+        <main class="medcode">
+          <div class="medcode__container">
+            <ion-label class="medcode__text" ds-color="neutral-10" token="p18x">Para ler uma marca, clique aqui:</ion-label>
+            <ion-button class="medcode__button" fill="clear" mode="ios" icon-only>
+              <ion-icon class="med-icon" slot="icon-only" name="med-qrcode"></ion-icon>
+            </ion-button>
+          </div>
+        </main>
+      </ion-content>
     </div>
   `;
 };
