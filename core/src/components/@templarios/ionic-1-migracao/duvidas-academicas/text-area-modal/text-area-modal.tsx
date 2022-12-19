a01c03d0858a2e2f6cc8e86a58fab775cc619c74
alanfedrizzi <alanfedrizzi@gmail.com>
2022-12-19 11:35:39 -0300

    ajueste modal text area migração

--- core/src/components/@templarios/ionic-1-migracao/duvidas-academicas/text-area-modal/text-area-modal.tsx
+++ core/src/components/@templarios/ionic-1-migracao/duvidas-academicas/text-area-modal/text-area-modal.tsx
@@ -10,23 +10,33 @@ export class TextAreaModal {
   render() {
     return (
       <Host>
-        <med-header>
-          <med-navbar class="header" slot="navbar">
-            <ion-back-button slot="left" mode="ios" text="" ds-size="xxs">
-              <ion-icon class="med-icon" name="med-fechar"></ion-icon>
-            </ion-back-button>
-
-            <ion-label class="header__title" slot="title">Depositar dúvida</ion-label>
-
-            <ion-button slot="right" mode="ios" fill="clear" icon-only ds-size="xs">
-              <ion-icon slot="icon-only" class="med-icon" name="med-enviar"></ion-icon>
-            </ion-button>
-          </med-navbar>
-        </med-header>
+        <ion-header class="header">
+          <div class="header__left">
+            <div>
+              <ion-button mode="ios" icon-only fill="clear" ds-size="xxs">
+                <ion-icon class="med-icon" name="med-fechar"></ion-icon>
+              </ion-button>
+            </div>
+          </div>
+        </ion-header>
 
         <ion-content class="modal-content">
           <div class="text-area">
-            <ion-textarea class="text-area__campo" mode="md" placeholder="Escreva a sua dúvida"></ion-textarea>
+            <ion-label class="text-area__title">
+              Depositar dúvida
+            </ion-label>
+
+            <ion-textarea
+              class="text-area__campo"
+              mode="md"
+              placeholder="Escreva a sua dúvida"
+            ></ion-textarea>
+
+            <div class="text-area__button-container">
+              <ion-button class="text-area__button" mode="ios" ds-size="sm">
+                Depositar
+              </ion-button>
+            </div>
           </div>
         </ion-content>
       </Host>
