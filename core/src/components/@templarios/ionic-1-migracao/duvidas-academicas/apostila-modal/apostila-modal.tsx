479b3b79837861ed272bc6f96595a64a35dbb35e
alanfedrizzi <alanfedrizzi@gmail.com>
2022-12-26 10:27:13 -0300

    migracao duvidas academicas ajuste modal apostilas

--- core/src/components/@templarios/ionic-1-migracao/duvidas-academicas/apostila-modal/apostila-modal.tsx
+++ core/src/components/@templarios/ionic-1-migracao/duvidas-academicas/apostila-modal/apostila-modal.tsx
@@ -10,68 +10,75 @@ export class ApostilaModal {
   render() {
     return (
       <Host>
-        <med-header>
-          <med-navbar class="header" slot="navbar">
-            <ion-button slot="left" mode="ios" fill="clear" icon-only ds-size="xxs">
-              <ion-icon class="med-icon" slot="icon-only" name="med-fechar"></ion-icon>
-            </ion-button>
-
-            <ion-label class="header__title" slot="title">Apostilas</ion-label>
-          </med-navbar>
-        </med-header>
+         <ion-header class="header">
+          <div class="header__left">
+            <div>
+              <ion-button mode="ios" icon-only fill="clear" ds-size="xxs">
+                <ion-icon class="med-icon" name="med-fechar"></ion-icon>
+              </ion-button>
+            </div>
+          </div>
+        </ion-header>
 
         <ion-content class="modal-content">
-          <div class="modal-content__input">
-            <tp-input-container has-icon="start">
-              <ion-icon class="med-icon" slot="start" name="med-busca"></ion-icon>
-              <ion-input mode="md" placeholder="Procurar o termo ou palavra-chave"></ion-input>
-            </tp-input-container>
-          </div>
+          <div class="modal-apostilas">
+            <ion-label class="modal-apostilas__title">
+              Apostilas
+            </ion-label>
 
-            <ion-item class="modal-content__opcoes" mode="ios" lines="none">
+            <div class="modal-apostilas__input">
+              <tp-input-container has-icon="start">
+                <ion-icon class="med-icon" slot="start" name="med-busca"></ion-icon>
+                <ion-input mode="md" placeholder="Procurar o termo ou palavra-chave"></ion-input>
+              </tp-input-container>
+            </div>
+
+            <ion-item class="modal-apostilas__opcoes" mode="ios" lines="none" ds-color="neutral-15">
               <ion-label>CAR 01</ion-label>
             </ion-item>
-            <ion-item class="modal-content__opcoes" mode="ios" lines="none">
+            <ion-item class="modal-apostilas__opcoes" mode="ios" lines="none" ds-color="neutral-15">
               <ion-label>CAR 02</ion-label>
             </ion-item>
-            <ion-item class="modal-content__opcoes" mode="ios" lines="none">
+            <ion-item class="modal-apostilas__opcoes" mode="ios" lines="none" ds-color="neutral-15">
               <ion-label>CAR 03</ion-label>
             </ion-item>
-            <ion-item class="modal-content__opcoes" mode="ios" lines="none">
+            <ion-item class="modal-apostilas__opcoes" mode="ios" lines="none" ds-color="neutral-15">
               <ion-label>CIR 01</ion-label>
             </ion-item>
-            <ion-item class="modal-content__opcoes" mode="ios" lines="none">
+            <ion-item class="modal-apostilas__opcoes" mode="ios" lines="none" ds-color="neutral-15">
               <ion-label>CIR 02</ion-label>
             </ion-item>
-            <ion-item class="modal-content__opcoes" mode="ios" lines="none">
+            <ion-item class="modal-apostilas__opcoes" mode="ios" lines="none" ds-color="neutral-15">
               <ion-label>CIR 03</ion-label>
             </ion-item>
-            <ion-item class="modal-content__opcoes" mode="ios" lines="none">
+            <ion-item class="modal-apostilas__opcoes" mode="ios" lines="none" ds-color="neutral-15">
               <ion-label>GO 01</ion-label>
             </ion-item>
-            <ion-item class="modal-content__opcoes" mode="ios" lines="none">
+            <ion-item class="modal-apostilas__opcoes" mode="ios" lines="none" ds-color="neutral-15">
               <ion-label>GO 02</ion-label>
             </ion-item>
-            <ion-item class="modal-content__opcoes" mode="ios" lines="none">
+            <ion-item class="modal-apostilas__opcoes" mode="ios" lines="none" ds-color="neutral-15">
               <ion-label>GO 03</ion-label>
             </ion-item>
-            <ion-item class="modal-content__opcoes" mode="ios" lines="none">
+            <ion-item class="modal-apostilas__opcoes" mode="ios" lines="none" ds-color="neutral-15">
               <ion-label>PED 01</ion-label>
             </ion-item>
-            <ion-item class="modal-content__opcoes" mode="ios" lines="none">
+            <ion-item class="modal-apostilas__opcoes" mode="ios" lines="none" ds-color="neutral-15">
               <ion-label>PED 02</ion-label>
             </ion-item>
-            <ion-item class="modal-content__opcoes" mode="ios" lines="none">
+            <ion-item class="modal-apostilas__opcoes" mode="ios" lines="none" ds-color="neutral-15">
               <ion-label>PED 03</ion-label>
             </ion-item>
 
+            <div class="modal-apostilas__button-container">
+              <ion-button class="modal-apostilas__button" mode="ios" ds-size="sm">
+                APLICAR
+              </ion-button>
+            </div>
+          </div>
         </ion-content>
 
-        <ion-footer class="modal-footer">
-          <ion-button class="modal-footer__button" mode="ios" ds-size="sm">
-            APLICAR
-          </ion-button>
-        </ion-footer>
+
       </Host>
     );
   }
