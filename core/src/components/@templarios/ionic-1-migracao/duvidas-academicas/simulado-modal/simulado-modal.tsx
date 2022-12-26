5bae39e56bebd8ac3dfd908585eb9e2cb7ff6328
alanfedrizzi <alanfedrizzi@gmail.com>
2022-12-26 09:54:18 -0300

    dúvidas acadêmicas ajuste modal simulados

--- core/src/components/@templarios/ionic-1-migracao/duvidas-academicas/simulado-modal/simulado-modal.tsx
+++ core/src/components/@templarios/ionic-1-migracao/duvidas-academicas/simulado-modal/simulado-modal.tsx
@@ -10,60 +10,67 @@ export class SimuladoModal {
   render() {
     return (
       <Host>
-        <med-header>
-          <med-navbar class="header" slot="navbar">
-            <ion-button slot="left" mode="ios" fill="clear" icon-only ds-size="xxs">
-              <ion-icon class="med-icon" slot="icon-only" name="med-fechar"></ion-icon>
-            </ion-button>
-
-            <ion-label class="header__title" slot="title">Simulados</ion-label>
-          </med-navbar>
-        </med-header>
+        <ion-header class="header">
+          <div class="header__left">
+            <div>
+              <ion-button mode="ios" icon-only fill="clear" ds-size="xxs">
+                <ion-icon class="med-icon" name="med-fechar"></ion-icon>
+              </ion-button>
+            </div>
+          </div>
+        </ion-header>
 
         <ion-content class="modal-content">
-          <ion-item class="modal-content__opcoes" mode="ios" lines="none">
-            <ion-label>2021 R1 SIM 01 - Geral 01</ion-label>
-          </ion-item>
-          <ion-item class="modal-content__opcoes" mode="ios" lines="none">
-            <ion-label>2021 R1 SIM 01 - Geral 02</ion-label>
-          </ion-item>
-          <ion-item class="modal-content__opcoes" mode="ios" lines="none">
-            <ion-label>2021 R1 SIM 01 - Geral 03</ion-label>
-          </ion-item>
-          <ion-item class="modal-content__opcoes" mode="ios" lines="none">
-            <ion-label>2021 R1 SIM 01 - Geral 04</ion-label>
-          </ion-item>
-          <ion-item class="modal-content__opcoes" mode="ios" lines="none">
-            <ion-label>2021 R1 SIM 01 - Geral 05</ion-label>
-          </ion-item>
-          <ion-item class="modal-content__opcoes" mode="ios" lines="none">
-            <ion-label>2021 R1 SIM 01 - Geral 06</ion-label>
-          </ion-item>
-          <ion-item class="modal-content__opcoes" mode="ios" lines="none">
-            <ion-label>2021 R1 SIM 01 - Geral 07</ion-label>
-          </ion-item>
-          <ion-item class="modal-content__opcoes" mode="ios" lines="none">
-            <ion-label>2021 R1 SIM 01 - UNIFESP</ion-label>
-          </ion-item>
-          <ion-item class="modal-content__opcoes" mode="ios" lines="none">
-            <ion-label>2021 R1 SIM 01 - UNIFESP</ion-label>
-          </ion-item>
-          <ion-item class="modal-content__opcoes" mode="ios" lines="none">
-            <ion-label>2021 R1 SIM 01 - UNIFESP</ion-label>
-          </ion-item>
-          <ion-item class="modal-content__opcoes" mode="ios" lines="none">
-            <ion-label>2021 R1 SIM 01 - UNIFESP</ion-label>
-          </ion-item>
-          <ion-item class="modal-content__opcoes" mode="ios" lines="none">
-            <ion-label>2021 R1 SIM 01 - UNIFESP</ion-label>
-          </ion-item>
+          <div class="modal-simulados">
+            <ion-label class="modal-simulados__title">
+              Simulados
+            </ion-label>
+            <ion-item class="modal-simulados__opcoes" mode="ios" lines="none" ds-color="neutral-15">
+              <ion-label>2021 R1 SIM 01 - Geral 01</ion-label>
+            </ion-item>
+            <ion-item class="modal-simulados__opcoes" mode="ios" lines="none" ds-color="neutral-15">
+              <ion-label>2021 R1 SIM 01 - Geral 02</ion-label>
+            </ion-item>
+            <ion-item class="modal-simulados__opcoes" mode="ios" lines="none" ds-color="neutral-15">
+              <ion-label>2021 R1 SIM 01 - Geral 03</ion-label>
+            </ion-item>
+            <ion-item class="modal-simulados__opcoes" mode="ios" lines="none" ds-color="neutral-15">
+              <ion-label>2021 R1 SIM 01 - Geral 04</ion-label>
+            </ion-item>
+            <ion-item class="modal-simulados__opcoes" mode="ios" lines="none" ds-color="neutral-15">
+              <ion-label>2021 R1 SIM 01 - Geral 05</ion-label>
+            </ion-item>
+            <ion-item class="modal-simulados__opcoes" mode="ios" lines="none" ds-color="neutral-15">
+              <ion-label>2021 R1 SIM 01 - Geral 06</ion-label>
+            </ion-item>
+            <ion-item class="modal-simulados__opcoes" mode="ios" lines="none" ds-color="neutral-15">
+              <ion-label>2021 R1 SIM 01 - Geral 07</ion-label>
+            </ion-item>
+            <ion-item class="modal-simulados__opcoes" mode="ios" lines="none" ds-color="neutral-15">
+              <ion-label>2021 R1 SIM 01 - UNIFESP</ion-label>
+            </ion-item>
+            <ion-item class="modal-simulados__opcoes" mode="ios" lines="none" ds-color="neutral-15">
+              <ion-label>2021 R1 SIM 01 - UNIFESP</ion-label>
+            </ion-item>
+            <ion-item class="modal-simulados__opcoes" mode="ios" lines="none" ds-color="neutral-15">
+              <ion-label>2021 R1 SIM 01 - UNIFESP</ion-label>
+            </ion-item>
+            <ion-item class="modal-simulados__opcoes" mode="ios" lines="none" ds-color="neutral-15">
+              <ion-label>2021 R1 SIM 01 - UNIFESP</ion-label>
+            </ion-item>
+            <ion-item class="modal-simulados__opcoes" mode="ios" lines="none" ds-color="neutral-15">
+              <ion-label>2021 R1 SIM 01 - UNIFESP</ion-label>
+            </ion-item>
+
+            <div class="modal-simulados__button-container">
+              <ion-button class="modal-simulados__button" mode="ios" ds-size="sm">
+                APLICAR
+              </ion-button>
+            </div>
+          </div>
         </ion-content>
 
-        <ion-footer class="modal-footer">
-          <ion-button class="modal-footer__button" mode="ios" ds-size="sm">
-            APLICAR
-          </ion-button>
-        </ion-footer>
+
       </Host>
     );
   }
