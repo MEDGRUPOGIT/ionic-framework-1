cc4a4d3148a4054fb55bbe46040456fec4e12f37
valdir-junior10 <valdir.conceicaojr@gmail.com>
2024-11-19 19:37:26 +0000

    Squashed commit of the following:
    commit 5356c67db0949c76cdc5adeb5197cfdc9cf35eed
    Author: valdir-junior10 <valdir.conceicaojr@gmail.com>
    Date:   Tue Nov 19 19:10:43 2024 +0000
    
        canClose med-alert

--- core/src/components/@templarios/core/med-alert/med-alert.tsx
+++ core/src/components/@templarios/core/med-alert/med-alert.tsx
@@ -34,20 +34,25 @@ export class MedAlert {
    */
   @Prop({ reflect: true }) disableSanitize = false;
 
+  /**
+  * todo
+  */
+  @Prop({ reflect: true }) canClose = true;
+
   dismiss(role: string) {
     modalController.dismiss(null, role);
   }
 
   render() {
-    const { heading, message, cancelText, confirmText, disableSanitize } = this;
+    const { heading, message, cancelText, confirmText, disableSanitize, canClose } = this;
 
     return (
       <Host>
-        <ion-header class="tp-dialog-header">
-            <ion-button mode="ios" icon-only fill="clear" ds-size="xxs" onClick={() => this.dismiss('close')}>
+        {canClose && <ion-header class="tp-dialog-header">
+             <ion-button mode="ios" icon-only fill="clear" ds-size="xxs" onClick={() => this.dismiss('close')}>
               <ion-icon slot="icon-only" class="med-icon" name="med-fechar"></ion-icon>
             </ion-button>
-          </ion-header>
+          </ion-header>}
           <div class="tp-dialog-container">
             <med-type class="tp-dialog-heading" token="h20x" innerHTML={!disableSanitize ? sanitizeDOMString(heading) : heading}></med-type>
             <med-type ds-color="neutral-8" token="h14x" innerHTML={!disableSanitize ? sanitizeDOMString(message) : message}></med-type>
